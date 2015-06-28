adf85e083e13a2b9fd338797782a9277e34e893a
Saulo Pratti <sppratti@gmail.com>
2015-06-28 15:34:14 -0300

    melhorado a inclusão de assets

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,14 @@ var elixir = require('laravel-elixir');
 var bowerDir = 'vendor/bower_components/';
 
 elixir(function(mix) {
+
     mix.less('app.less')
     .copy(bowerDir + 'normalize-css/normalize.css', 'public/css/vendor/normalize.css')
-    .copy(bowerDir + 'bootstrap/dist/css/bootstrap.css', 'public/css/vendor/bootstrap.css')
-    .version(['css/app.css'])
+    .copy(bowerDir + 'bootstrap/dist/css/bootstrap.css', 'public/css/vendor/bootstrap.css');
+
+    mix.styles([
+        'vendor/normalize.css', 'app.css'
+    ], null, 'public/css');
+
+    mix.version('public/css/all.css');  
 });
