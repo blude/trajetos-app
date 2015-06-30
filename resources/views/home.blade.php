e0c6be37cc0ac4ad41bdce49ebb55d85120346ba
Saulo Pratti <sppratti@gmail.com>
2015-06-30 00:20:46 -0300

    melhorado condicionais e adicionado links pra criar novo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,27 @@
 @section('content')
     <h1>Trajetos</h1>
     <p>Veja pontos de referência, fotos, mapas, vias e nunca mais desça no ponto errado. <a href="{{ url('/about') }}">Saiba mais</a>.</p>
-    <h2>Linhas:</h2>
+    
+    @unless ($routes->isEmpty())
+        <h2>Linhas:</h2>
+        <ul>
+            @foreach ($routes as $route)
+                <li>{!! link_to_route('routes.edit', $route->fullName(), $route->id) !!}</a></li>
+            @endforeach
+        </ul>
+    @else
+        <p>Nenhuma linha disponível. {!! link_to_route('routes.create', 'Adicionar Nova Linha') !!}</p>
+    @endunless
 
-    <ul>
-        @forelse ($routes as $route)
-            <li>{!! link_to_route('routes.edit', $route->fullName(), $route->id) !!}</a></li>
-        @empty
-            <li>Nenhuma linha disponível.</li>
-        @endforelse
-    </ul>
-    <h2>Viagens:</h2>
-    <ul>
-        @forelse ($trips as $trip)
-            <li>{!! link_to_route('trips.show', $trip->fullName(), $trip->id) !!}</li>
-        @empty
-            <li>Nenhuma viagem disponível.</li>
-        @endforelse
-    </ul
+    @unless ($routes->isEmpty())
+        <h2>Viagens:</h2>
+        <ul>
+            @foreach ($trips as $trip)
+                <li>{!! link_to_route('trips.show', $trip->fullName(), $trip->id) !!}</li>
+            @endforeach
+        </ul>
+    @else
+        <p>Nenhuma viagem disponível. {!! link_to_route('trips.create', 'Adicionar Nova Viagem') !!}</p>
+    @endunless
     <p>De Vitória para o mundo.</p>
 @stop
