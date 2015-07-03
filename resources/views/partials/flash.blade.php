a8e57750b8236bc3e040dcd8c18f1a2ddb3d0d94
Saulo Pratti <sppratti@gmail.com>
2015-07-02 23:32:19 -0300

    classe do alerta corrigido

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,7 +1,7 @@
 @if (session()->has('flash_message'))
-    <div class="alert alert-{{ session('flash_message_level') }} {{ session()->has('flash_message_important') ? 'is-dismissable' : '' }}" role="alert">
+    <div class="alert alert-{{ session('flash_message_level') }} {{ session()->has('flash_message_important') ? 'alert-dismissable' : '' }}" role="alert">
         @if (session()->has('flash_message_important'))
-            <button class="close" type="button" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
+            <button class="close" type="button" data-dismiss="alert" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
         @endif
         <p>{{ session('flash_message') }}</p>
     </div>
