10f17792e01379faed411bb22bdb448c73d8203d
Saulo Pratti <sppratti@gmail.com>
2015-07-11 01:41:22 -0300

    Ponto atualizado com relacionamentos a outras tabelas

--- app/Point.php
+++ app/Point.php
@@ -32,6 +32,46 @@ class Point extends Model
     }
 
     /**
+     * The point can be a bus stop.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function bus_stop()
+    {
+        return $this->hasOne('App\BusStop');
+    }
+
+    /**
+     * The point can be a landmark.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function landmark()
+    {
+        return $this->hasOne('App\Landmark');
+    }
+
+    /**
+     * The point can be a street view.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function street_view()
+    {
+        return $this->hasOne('App\StreetView');
+    }
+
+    /**
+     * The point can be a turn.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function turn()
+    {
+        return $this->hasOne('App\Turn');
+    }
+
+    /**
      * Get a list of trip ids associated with the current point.
      *
      * @return array
