13136f924f6ee04adf028194a73f94a034738cd4
Saulo Pratti <sppratti@gmail.com>
2015-07-12 22:50:13 -0300

    só pra view funcionar

--- resources/views/bus_stops/show.blade.php
+++ resources/views/bus_stops/show.blade.php
@@ -1,41 +1,5 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Ponto <small>#{{ $point->id }}</small></h1>
-    <table class="table table-striped">
-        <thead>
-            <tr>                
-                <th>ID</th>
-                <th>Latitude</th>
-                <th>Longitude</th>
-                <th>Tipo</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr data-point-id="{{ $point->id }}">
-                <td>{{ $point->id }}</td>
-                <td>{{ $point->lat }}</td>
-                <td>{{ $point->lon }}</td>
-                <td>{{ $point->point_type->label }}</td>
-                <td>
-                    {!! link_to_route('points.show', 'ver', $point->id) !!} - 
-                    {!! link_to_route('points.edit', 'editar', $point->id) !!}
-                </td>
-            </tr>
-        </tbody>
-    </table>
-@stop
-
-@section ('sidebar')
-    <h2>Linhas</h2>
-    @unless ($point->trips->isEmpty())
-        <ul>
-            @foreach ($point->trips->all() as $trip)
-                <li>{{ $trip->fullName() }}</li>
-            @endforeach
-        </ul>
-    @else
-        <p class="text-muted">Nenhuma linha cadastrada.</p>
-    @endunless
+    <h1>Parada <small>{{ $bus_stop->number }}</small></h1>
 @stop
