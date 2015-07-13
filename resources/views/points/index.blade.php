fd803ffa3579ad40923173b1caa40c53c370f3b6
Saulo Pratti <sppratti@gmail.com>
2015-07-12 23:35:03 -0300

    listagem das viagens de cada ponto

--- resources/views/points/index.blade.php
+++ resources/views/points/index.blade.php
@@ -9,6 +9,7 @@
                 <th>Latitude</th>
                 <th>Longitude</th>
                 <th>Tipo</th>
+                <th>Viagem</th>
                 <th>Ações</th>
             </tr>
         </thead>
@@ -20,12 +21,17 @@
                         <td>{{ $point->lat }}</td>
                         <td>{{ $point->lon }}</td>
                         <td>{{ $point->point_type->label }}</td>
+                        <td>
+                            @foreach ($point->trips as $trip)
+                               <div>{{ $trip->fullName() }}</div>
+                            @endforeach
+                        </td>
                         <td>{!! link_to_route('points.show', 'ver', $point->id) !!} - {!! link_to_route('points.edit', 'editar', $point->id) !!}</td>
                     </tr>
                 @endforeach
             @else
                 <tr>
-                    <td class="text-center text-muted" colspan="5">Nenhum ponto cadastrado.</td>
+                    <td class="text-center text-muted" colspan="6">Nenhum ponto cadastrado.</td>
                 </tr>
             @endunless
         </tbody>
