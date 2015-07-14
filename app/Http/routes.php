678f2b4f79327adaef18dc58cfaee0fd8afbadfb
Saulo Pratti <sppratti@gmail.com>
2015-07-13 23:15:24 -0300

    agrupamento de rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -38,19 +38,29 @@ Route::group(['prefix' => 'admin'], function() {
 |--------------------------------------------------------------------------
 */
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::group(['namespace' => 'Auth'], function()
+{
+    Route::group(['prefix' => 'auth'], function()
+    {
+        // Authentication routes...
+        Route::get('login', 'AuthController@getLogin');
+        Route::post('login', 'AuthController@postLogin');
+        Route::get('logout', 'AuthController@getLogout');
 
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+        // Registration routes...
+        Route::get('register', 'AuthController@getRegister');
+        Route::post('register', 'AuthController@postRegister');        
+    });
 
-// Password reset link request routes...
-Route::get('password/email', 'Auth\PasswordController@getEmail');
-Route::post('password/email', 'Auth\PasswordController@postEmail');
+    Route::group(['prefix' => 'password'], function()
+    {
+        // Password reset link request routes...
+        Route::get('email', 'PasswordController@getEmail');
+        Route::post('email', 'PasswordController@postEmail');
 
-// Password reset routes...
-Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
-Route::post('password/reset', 'Auth\PasswordController@postReset');
+        // Password reset routes...
+        Route::get('reset/{token}', 'PasswordController@getReset');
+        Route::post('reset', 'PasswordController@postReset');
+    });
+});
+
